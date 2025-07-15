7d26236744b58c9646ca3181d1e6a787748a0aed
JJG <gaoyuan961021@gmail.com>
2025-07-15 12:38:56 +0800

    Update api-key.js

--- QUAM/frontend/api-key.js
+++ QUAM/frontend/api-key.js
@@ -1,14 +1,17 @@
 const API_KEYS = [
-  '543f0286dcmsh50a65d23af85fe7p11666fjsnd26d12d6e6b8',
-  '790c957f1cmsha9913d670fe58cbp11a8a0jsnbf4d2d9929ba',
-  '9f6384f00amsh216bb458e5b2ef6p19b738jsn366cb8242758'
+  '543f0286dcmsh50a65d23af85fe7p11666fjsnd26d12d6e6b8', //jasmine.gao@yololab.io
+  '790c957f1cmsha9913d670fe58cbp11a8a0jsnbf4d2d9929ba', //gaoyuan961021@gmail.com
+  '9f6384f00amsh216bb458e5b2ef6p19b738jsn366cb8242758', //
+  '145ec599b8mshd70bc43c884e9c9p176643jsn4bb1cb4ff84d' //saas90169@gmail.com
   // Add more API keys here as needed
 ];
 
 const AUDIO_API_KEYS = [
   // Add your audio RapidAPI keys here, e.g.:
-  '9f6384f00amsh216bb458e5b2ef6p19b738jsn366cb8242758',
   '543f0286dcmsh50a65d23af85fe7p11666fjsnd26d12d6e6b8',
+  '790c957f1cmsha9913d670fe58cbp11a8a0jsnbf4d2d9929ba',
+  '9f6384f00amsh216bb458e5b2ef6p19b738jsn366cb8242758',
+  '145ec599b8mshd70bc43c884e9c9p176643jsn4bb1cb4ff84d'
 ];
 
 export { API_KEYS, AUDIO_API_KEYS }; 
