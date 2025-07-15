84a3d2ef459241cc7d468e1472e2c99b067c74b2
JJG <gaoyuan961021@gmail.com>
2025-07-15 12:42:57 +0800

    Update api-key.js

--- QUAM/frontend/api-key.js
+++ QUAM/frontend/api-key.js
@@ -1,8 +1,8 @@
 const API_KEYS = [
-  '543f0286dcmsh50a65d23af85fe7p11666fjsnd26d12d6e6b8', //jasmine.gao@yololab.io
-  '790c957f1cmsha9913d670fe58cbp11a8a0jsnbf4d2d9929ba', //gaoyuan961021@gmail.com
+  '543f0286dcmsh50a65d23af85fe7p11666fjsnd26d12d6e6b8', //jasmine.gao
+  '790c957f1cmsha9913d670fe58cbp11a8a0jsnbf4d2d9929ba', //gaoyuan961021
   '9f6384f00amsh216bb458e5b2ef6p19b738jsn366cb8242758', //
-  '145ec599b8mshd70bc43c884e9c9p176643jsn4bb1cb4ff84d' //saas90169@gmail.com
+  '145ec599b8mshd70bc43c884e9c9p176643jsn4bb1cb4ff84d' //saas90169
   // Add more API keys here as needed
 ];
 
